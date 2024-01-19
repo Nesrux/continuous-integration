e85d185c9d8edcd7b4da34cc17794c40fc26b338
Nesrux <joaomarcosdevs@gmail.com>
2024-01-19 19:28:29 -0300

    feat: Adicionando o arquivo de Test para arquivo main.go

---
+++ main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestMain(t *testing.T) {
+	total := Somar(15, 15)
+
+	if total != 30 {
+		t.Errorf("Resultado da soma é inválido: o Resultado %d," +
+		 "não é o valor esperado", total)
+	}
+}
