d85b5a13d7077a04e6f5faf0b4b886404342ab83
Nesrux <joaomarcosdevs@gmail.com>
2024-01-19 19:17:46 -0300

    feat: Criando arquivo main.go para teste de CI

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 10
+	c := Somar(a, b)
+	fmt.Printf("A Soma de A + B é %d", c)
+}
+func Somar(a, b int) int {
+	return a + b
+}
--- math.go
+++
@@ -1,5 +0,0 @@
-package soma
-
-func somar(a, b *int) int {
-	return a + b
-}
