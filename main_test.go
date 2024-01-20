762c8019f47181c1c529ad661934d93d013c100c
Nesrux <joaomarcosdevs@gmail.com>
2024-01-20 17:02:09 -0300

    feat: refatorando o arquivo de test

--- main_test.go
+++ main_test.go
@@ -5,8 +5,8 @@ import "testing"
 func TestMain(t *testing.T) {
 	total := Somar(15, 15)
 
-	if total != 30 {
-		t.Errorf("Resultado da soma é inválido: o Resultado %d," +
-		 "não é o valor esperado", total)
+	if total != 31 {
+		t.Errorf("Resultado da soma é inválido: o Resultado %d,"+
+			"não é o valor esperado", total)
 	}
 }
