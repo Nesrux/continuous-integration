e40661c04a4477b81f482e00c97f8018a4d8aeb7
Nesrux <joaomarcosdevs@gmail.com>
2024-01-20 17:05:52 -0300

    fix: removendo erro de implementação do código

--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestMain(t *testing.T) {
 	total := Somar(15, 15)
 
-	if total != 31 {
+	if total != 30 {
 		t.Errorf("Resultado da soma é inválido: o Resultado %d,"+
 			"não é o valor esperado", total)
 	}
